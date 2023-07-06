2cabbd8fddac0e3d08709fc6228cf4edcf6a639d
Ilya_Mishin <Masdofan@yandex.ru>
2023-07-06 16:07:44 +0300

    Hometask3 loop

---
+++ Hometask3/src/main/java/HT6from5to1.java
@@ -0,0 +1,13 @@
+public class HT6from5to1 {
+
+    public static void main(String[] args) {
+
+        //Необходимо вывести на экран числа от 5 до 1.
+
+        int i = 6;
+        while (i > 1){
+            i = --i;
+            System.out.println(i);
+        }
+    }
+}
