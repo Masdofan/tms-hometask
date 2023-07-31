b6b010b72299c6300c7f2012778fd64e4322a892
Ilya Mishin <Masdofan@yandex.ru>
2023-07-31 03:57:39 +0300

    Hometask10 v1

---
+++ Hometask10/src/main/java/HT1/WorkWithFiles.java
@@ -0,0 +1,54 @@
+package HT1;
+
+import java.io.*;
+
+public class WorkWithFiles {
+
+    public void checkDocValid(File file, File pathForValidReports, File pathForNonValidReports) {
+
+        try (LineNumberReader reader = new LineNumberReader(new FileReader(file));
+             Writer validWriter = new FileWriter(pathForValidReports);
+             Writer nonValidWriter = new FileWriter(pathForNonValidReports)){
+
+            String s = reader.readLine();
+
+
+
+            while (s != null) {
+                System.out.println(s);
+
+                if (s.length() == 15) {
+                    if (s.startsWith("docnum") || s.startsWith("contract")){
+                        System.out.println("Document number is valid");
+                        validWriter.write("\n" + s);
+                    } else {
+                        System.out.println("Document number is not valid");
+                        nonValidWriter.write("\n" + s + ": don't starts with docnum or contract");
+                    }
+                } else {
+                    System.out.println("Document number is not valid");
+                    nonValidWriter.write("\n" + s + ": have more or less 15 chars");
+                }
+                s = reader.readLine();
+            }
+
+        } catch (IOException exc) {
+
+        }
+
+    }
+
+    public void docNumsReport(File pathForValidReports, File pathForNonValidReports) {
+
+        try (Writer validWriter = new FileWriter(pathForValidReports);
+             Writer nonValidWriter = new FileWriter(pathForNonValidReports)) {
+
+
+
+        }catch (IOException exc) {
+
+        }
+
+    }
+
+}
