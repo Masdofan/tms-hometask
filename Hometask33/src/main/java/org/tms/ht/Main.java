2a092fc88dc4d4006814d6ce55d7e361b0b82d02
Ilya_Mishin <Masdofan@yandex.ru>
2023-11-12 15:06:52 +0300

    Hometask33 one more fix

--- Hometask33/src/main/java/org/tms/ht/Main.java
+++ Hometask33/src/main/java/org/tms/ht/Main.java
@@ -1,16 +1,18 @@
 package org.tms.ht;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.tms.ht.config.HibernateConfig;
 import org.tms.ht.entity.TaskEntity;
 import org.tms.ht.entity.UserEntity;
+import org.tms.ht.service.TaskService;
+import org.tms.ht.service.UserService;
 
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        TaskService taskService = new TaskService();
+        UserService userService = new UserService();
 
         UserEntity user1 = UserEntity.builder()
                 .name("user1")
@@ -26,8 +28,8 @@ public class Main {
                 .isMan(false)
                 .build();
 
-        UserEntity.save(user1);
-        UserEntity.save(user2);
+        userService.save(user1);
+        userService.save(user2);
 
 
         TaskEntity task1 = TaskEntity.builder()
@@ -35,40 +37,40 @@ public class Main {
                 .description("desc1")
                 .status(TaskStatus.NEW)
                 .build();
-        TaskEntity.save(task1);
+        taskService.save(task1);
 
         TaskEntity task2 = TaskEntity.builder()
                 .title("title2")
                 .description("desc2")
                 .status(TaskStatus.IN_PROGRESS)
                 .build();
-        TaskEntity.save(task2);
+        taskService.save(task2);
 
         TaskEntity task3 = TaskEntity.builder()
                 .title("title3")
                 .description("desc3")
                 .status(TaskStatus.DONE)
                 .build();
-        TaskEntity.save(task3);
+        taskService.save(task3);
 
         TaskEntity task4 = TaskEntity.builder()
                 .title("title4")
                 .description("desc4")
                 .status(TaskStatus.IN_PROGRESS)
                 .build();
-        TaskEntity.save(task4);
+        taskService.save(task4);
 
-        TaskEntity.addTaskToUser(1,1);
-        TaskEntity.addTaskToUser(2,1);
-        TaskEntity.addTaskToUser(3,2);
-        TaskEntity.addTaskToUser(4,2);
+        taskService.addTaskToUser(1,1);
+        taskService.addTaskToUser(2,1);
+        taskService.addTaskToUser(3,2);
+        taskService.addTaskToUser(4,2);
 
-        TaskEntity.updateTaskStatus(1, TaskStatus.IN_PROGRESS);
-        TaskEntity.updateTaskStatus(4, TaskStatus.DONE);
+        taskService.updateTaskStatus(1, TaskStatus.IN_PROGRESS);
+        taskService.updateTaskStatus(4, TaskStatus.DONE);
 
-        System.out.println(TaskEntity.getByActiveTasks());
+        System.out.println(taskService.getByActiveTasks());
 
-        UserEntity.deleteAll();
+        userService.deleteAll();
 
     }
 
---
+++ Hometask33/src/main/java/org/tms/ht/service/UserService.java
@@ -0,0 +1,120 @@
+package org.tms.ht.service;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.tms.ht.UserRole;
+import org.tms.ht.config.HibernateConfig;
+import org.tms.ht.entity.UserEntity;
+
+import java.util.Date;
+import java.util.List;
+
+public class UserService {
+
+    public void save(UserEntity user) {
+        Session session = HibernateConfig.create();
+        Transaction transaction = session.beginTransaction();
+
+        session.save(user);
+
+        transaction.commit();
+        session.close();
+    }
+
+    public UserEntity getById (int id) {
+        Session session = HibernateConfig.create();
+        Transaction transaction = session.beginTransaction();
+
+        UserEntity userEntity = session.find(UserEntity.class, id);
+
+        transaction.commit();
+        session.close();
+        return userEntity;
+    }
+
+    public void deleteById (int id) {
+        Session session = HibernateConfig.create();
+        Transaction transaction = session.beginTransaction();
+
+        UserEntity userEntity = session.find(UserEntity.class, id);
+        session.delete(userEntity);
+
+        transaction.commit();
+        session.close();
+    }
+
+    public void updateName (int userId, String newName) {
+        Session session = HibernateConfig.create();
+        Transaction transaction = session.beginTransaction();
+
+        UserEntity userEntity = session.find(UserEntity.class, userId);
+        userEntity.setName(newName);
+
+        session.update(userEntity);
+
+        transaction.commit();
+        session.close();
+
+    }
+
+    public void updateRole (int userId, UserRole role) {
+        Session session = HibernateConfig.create();
+        Transaction transaction = session.beginTransaction();
+
+        UserEntity userEntity = session.find(UserEntity.class, userId);
+        userEntity.setRole(role);
+
+        session.update(userEntity);
+
+        transaction.commit();
+        session.close();
+
+    }
+
+    public void updateBirthday (int userId, Date newBirthday) {
+        Session session = HibernateConfig.create();
+        Transaction transaction = session.beginTransaction();
+
+        UserEntity userEntity = session.find(UserEntity.class, userId);
+        userEntity.setBirthday(newBirthday);
+
+        session.update(userEntity);
+
+        transaction.commit();
+        session.close();
+
+    }
+
+    public void updateGender (int userId, boolean isMan) {
+        Session session = HibernateConfig.create();
+        Transaction transaction = session.beginTransaction();
+
+        UserEntity userEntity = session.find(UserEntity.class, userId);
+        userEntity.setMan(isMan);
+
+        session.update(userEntity);
+
+        transaction.commit();
+        session.close();
+
+    }
+
+    public void deleteAll() {
+        Session session = HibernateConfig.create();
+        Transaction transaction = session.beginTransaction();
+
+        Query query = session.createQuery("from UserEntity");
+        List list = query.list();
+
+        for (var item : list) {
+
+            session.delete(item);
+
+        }
+
+        transaction.commit();
+        session.close();
+    }
+
+}
